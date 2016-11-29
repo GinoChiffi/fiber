a7f5afe47fcf03da7fc44b0022878ef3fc0e0760
Gino Chiffi <gino.chiffi@gmail.com>
2016-11-29 17:27:28 +0100

    Add item_like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129162444) do
+ActiveRecord::Schema.define(version: 20161129162624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20161129162444) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "item_likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_item_likes_on_item_id", using: :btree
+    t.index ["user_id"], name: "index_item_likes_on_user_id", using: :btree
   end
 
   create_table "items", force: :cascade do |t|
@@ -75,6 +84,8 @@ ActiveRecord::Schema.define(version: 20161129162444) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "item_likes", "items"
+  add_foreign_key "item_likes", "users"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
