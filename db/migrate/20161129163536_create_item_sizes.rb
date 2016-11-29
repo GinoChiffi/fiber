c5f6a45caca8f19a872b11080a91ebcc78849de2
Gino Chiffi <gino.chiffi@gmail.com>
2016-11-29 17:36:47 +0100

    Add item_size  model

---
+++ db/migrate/20161129163536_create_item_sizes.rb
@@ -0,0 +1,10 @@
+class CreateItemSizes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :item_sizes do |t|
+      t.references :size, foreign_key: true
+      t.references :item, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129163409) do
+ActiveRecord::Schema.define(version: 20161129163536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 20161129163409) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_item_likes_on_item_id", using: :btree
     t.index ["user_id"], name: "index_item_likes_on_user_id", using: :btree
+  end
+
+  create_table "item_sizes", force: :cascade do |t|
+    t.integer  "size_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_item_sizes_on_item_id", using: :btree
+    t.index ["size_id"], name: "index_item_sizes_on_size_id", using: :btree
   end
 
   create_table "items", force: :cascade do |t|
@@ -110,6 +119,8 @@ ActiveRecord::Schema.define(version: 20161129163409) do
   add_foreign_key "item_colors", "items"
   add_foreign_key "item_likes", "items"
   add_foreign_key "item_likes", "users"
+  add_foreign_key "item_sizes", "items"
+  add_foreign_key "item_sizes", "sizes"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
