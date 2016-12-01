161027cc1225c1893333e893c3e7e5acbba61f27
Gino Chiffi <gino.chiffi@gmail.com>
2016-12-01 12:27:42 +0100

    Add routes to models

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,26 @@ Rails.application.routes.draw do
 
   get '/home', to: 'static_pages#home'
 
+  resources :items, only: [:index, :show] do
+  resources :item_likes, only: [:create, :destroy]
+  resources :orders, only: [:create, :destroy]
+  end
+
+  resources :profiles, only: [:show] do
+    member do
+    patch 'follow'
+    patch 'unfollow'
+  end
+  end
+
+resource :dashboard, only: [:show, :edit, :update]
+resource :settings, only: [:show, :edit, :update]
+
+
+
+
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
