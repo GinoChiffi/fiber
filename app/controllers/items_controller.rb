54920bd7087eda91ea1ca06590d5fe2237856664
Gino Chiffi <gino.chiffi@gmail.com>
2016-12-02 16:55:11 +0100

    Add min max price search filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,13 +3,16 @@ class ItemsController < ApplicationController
 
 
 def index
-  @items = Item.all
 
   if params[:search]
     @items = Item.search(params[:search]).order("created_at DESC")
   else
     @items = Item.all.order("created_at DESC")
   end
+
+  #this is filter for price
+  @items.to_a.select!{ |item| (item.price > params[:min_price].to_f) && (item.price < params[:max_price].to_f) }
+
 end
 
 
