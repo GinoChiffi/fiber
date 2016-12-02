144cba218b5a52417877bc84936e022e5a737e49
Gino Chiffi <gino.chiffi@gmail.com>
2016-12-02 12:13:17 +0100

    Add unlike functionality

--- app/controllers/item_likes_controller.rb
+++ app/controllers/item_likes_controller.rb
@@ -11,6 +11,18 @@ class ItemLikesController < ApplicationController
   end
 
   def destroy
+
+    if user_signed_in?
+      @item_like = ItemLike.where(user_id: current_user.id, item_id: params[:item_id]).first
+      if @item_like.present?
+        @item_like.destroy
+        redirect_to :back, notice: 'You unliked item!'
+      else
+        redirect_to :back, alert: 'You have no liked item!'
+      end
+    else
+      redirect_to :back, alert: 'You need to be logged in to like item!'
+    end
   end
 
 end
