9361b67e41bdb8c761704af149d26d41f83c9707
Gino Chiffi <gino.chiffi@gmail.com>
2016-12-02 11:44:39 +0100

    Add like functionality

--- app/controllers/item_likes_controller.rb
+++ app/controllers/item_likes_controller.rb
@@ -1,7 +1,12 @@
 class ItemLikesController < ApplicationController
   def create
-    @item_like = ItemLike.new
-    if @item_like.save
+    if user_signed_in?
+
+      ItemLike.where(user_id: current_user.id, item_id: params[:item_id]).first_or_create!
+      redirect_to :back, notice: 'You liked the item!'
+    else
+      redirect_to :back, alert: 'You need to be logged in to like an item!'
+    end
 
   end
 
