e4d4aa91aa1fa0339b3d2ade250230acc4bc7d37
Gino Chiffi <gino.chiffi@gmail.com>
2016-12-13 18:35:58 +0100

    Add order fucntionality to order index etc

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,9 @@ class OrdersController < ApplicationController
     @sizes = Size.joins(:item_sizes)
                  .where(item_sizes: { item_id: @item_id })
                  .map { |s| [s.value.upcase, s.id] }
+    @colors = Color.joins(:item_colors)
+                   .where(item_colors: { item_id: @item_id})
+                   .map { |c| [c.name.capitalize, c.id] }
   end
 
   def create
@@ -17,7 +20,7 @@ class OrdersController < ApplicationController
     if @order.save
       redirect_to orders_path
     else
-      # do that
+      render :new
     end
   end
 
@@ -28,7 +31,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:price, :pick_up_date, :item_id, :size_id)
+    params.require(:order).permit(:price, :pick_up_date, :item_id, :size_id, :color_id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213145659) do
+ActiveRecord::Schema.define(version: 20161213162934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20161213145659) do
     t.datetime "updated_at",                   null: false
     t.boolean  "completed",    default: false
     t.integer  "size_id"
+    t.integer  "color_id"
     t.index ["item_id"], name: "index_orders_on_item_id", using: :btree
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
