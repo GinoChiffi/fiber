6b69f04bb128f307d97a853aac43a6911b720491
Donald van Eupen <dovaneupen@gmail.com>
2016-12-15 13:17:58 +0100

    Shop: Name Uniquess Scoped To City

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,8 +1,6 @@
 class Shop < ApplicationRecord
   has_many :items
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :city }
   validates :city, presence: true
-  # validates :image_shop, presence: true
-  # validates :description, presence: true
 end
